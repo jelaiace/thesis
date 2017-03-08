a8ed0bba67c3dfbfbbbd94a3103ad09f3446a6d2
srph <seraphipod@gmail.com>
2017-03-09 03:01:24 +0800

    Display professor's schedules

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,16 +3,50 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+use App\Schedule;
+use App\User;
+use App\Room;
+use App\Subject;
+use App\Block;
+use App\Department;
 
 class HomeController extends Controller
 {
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return view('home');
-    }
+  /**
+   * Show the application dashboard.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index(Request $request)
+  {
+  	$user = Auth::user();
+
+  	if ($user->type === 'professor') {
+      $professors = User::where('type', 'professor')->get();
+      $subjects = Subject::all();
+      $blocks = Block::all();
+      $day = strtolower($request->get('day', 'mth'));
+      $rooms = Room::join('schedules', function($join) use ($day) {
+      	$join->on('rooms.id', '=', 'schedules.room_id')
+      		->where('day', $day);
+      })->with(
+        'schedules.professor',
+        'schedules.block',
+        'schedules.room',
+        'schedules.subject'
+      )->get();
+
+      return view('index-professor', compact(
+        'professors',
+        'subjects',
+        'blocks',
+        'day',
+        'rooms'
+      ));
+  	}
+
+    return view('home');
+  }
 }
